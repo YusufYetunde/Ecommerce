c7dcaa420780ac32601cfe7dd4474a7797abf03d
Yusuf Yetunde <yusufyetunde13@gmail.com>
2022-03-04 15:37:55 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [productCount, setProductCount] = useState(0);
+  const add = () => setProductCount((old) => ++old);
+  const sub = () => setProductCount((old) => --old);
+
+  return (
+    <div className="App">
+      <Header productCount={productCount} />
+      <Main productCount={productCount} add={add} sub={sub} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavCartImage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../style/header.module.css";
+import { Popover } from "react-tiny-popover";
+import { useState } from "react";
+import Cart from "./Cart";
+export default function NavCartImage({ productCount }) {
+  const [isCartShowing, setIsCartShowing] = useState(false);
+
+  return (
+    <div className={styles.navCartList}>
+      {/* Cart  */}
+      <Popover
+        isOpen={isCartShowing}
+        positions={["bottom"]} // preferred positions by priority
+        content={<Cart productCount={productCount} />}
+      >
+        <div onClick={() => setIsCartShowing(!isCartShowing)}>
+          {" "}
+          <img alt="" className={styles.navCart} src="./images/icon-cart.svg" />
+        </div>
+      </Popover>
+
+      {/* profile */}
+
+      <img
+        className={styles.navAvatar}
+        alt=""
+        src="./images/image-avatar.png"
+      />
+    </div>
+  );
+}
